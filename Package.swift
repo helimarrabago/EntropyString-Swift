48f9a8bfd1e7e44d628efed5c681f3a60d71d7ee
Helimar Rabago <heli@nueca.com.ph>
2023-06-21 11:51:09 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,5 +3,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "EntropyString"
+    name: "EntropyString",
+    products: [
+        .library(
+            name: "EntropyString",
+            targets: ["EntropyString"])
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "EntropyString",
+            dependencies: [],
+            path: "Sources"),
+        .testTarget(
+            name: "EntropyStringTests",
+            dependencies: ["EntropyString"])
+    ]
 )
